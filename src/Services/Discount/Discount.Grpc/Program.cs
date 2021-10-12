a3177be2b89d42c7f36c307e5704ac353c16dfd8
lanrex52 <lanrex52@gmail.com>
2021-10-12 13:01:29 +0100

    changes

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,17 +1,24 @@
-using Discount.Grpc.Services;
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repository;
+//using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var host = WebApplication.Create(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+//Migrate database
+host.MigrateDatabase();
+//Register Dependency Injection
 
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 // Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
